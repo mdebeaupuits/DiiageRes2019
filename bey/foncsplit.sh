eea4882418e8db37fb8a34cfcf755a8c841cd035
Clement BEY <clement.bey@diiage.org>
2018-02-20 08:54:40 +0100

    Test 1

---
+++ bey/foncsplit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# NOTE : NE PAS UTILISER, NON TERMINE
+#echo ${#phrase}
+#echo ${phrase} |tr ":" "\n"
+fonction Decoup ()
+	{ phrase="Bonjour : ceci est une phrase"
+	  delim =":"
+	  cut -d ${2} 
+	}
