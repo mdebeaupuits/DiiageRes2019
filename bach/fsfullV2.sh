052ed0e671e8f6f14c84d93583a09738d3b5da6b
Bach <kevin.bach@diiage.org>
2018-04-05 12:34:09 +0200

    script demandé le 05/04/2018

--- bach/fsfullV2.sh
+++ bach/fsfullV2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#fsfull.sh
+#fsfullV2.sh
 #Description : script servant à afficher une alerte si l'un des filesystem dépasse 80% d'utilisation.
 
 df -m |grep -v fichiers| awk '{ if($5 > 80) print "Alerte de saturation à plus de 80% sur" $6; }'	
