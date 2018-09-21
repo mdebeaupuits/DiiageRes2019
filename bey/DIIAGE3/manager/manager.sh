92f53fda1a14bef6d23bf18dfde406489eb1589f
Clement BEY <clement.bey@diiage.org>
2018-09-21 14:59:30 +0200

    ajout ssh, verification ip

--- bey/DIIAGE3/manager/manager.sh
+++ bey/DIIAGE3/manager/manager.sh
@@ -6,6 +6,23 @@ basemachine=data.txt
 echo "Que souhaitez vous faire ? (Ajouter un hote(1), modifier un hote (2) ou supprimer (3)) ?"
 read choice
 
+function verif_ip () {
+	if [[ "$ip" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
+  		echo ""
+	else
+  		echo "fail, entrer une adresse IP valide"
+		exit
+	fi
+}
+
+function ssh_timeout () {
+                ssh -o ConnectTimeout=10 ${user}@${ip}
+                if [[ $? -gt 1 ]]; then
+                echo "fail, erreur dans l'adresse IP ou port non ouvert ?"
+                exit
+                fi
+}
+
 function add () {
 	awk "BEGIN  {i=1} /${ADD}/ {i=0} END {exit i}" ${basemachine};
 	if [[ $? -eq 0 ]]
@@ -13,6 +30,7 @@ function add () {
 		echo "La machine est deja presente dans la base"
 		return 1
 	fi
+
 	echo "${ADD}" >> ${basemachine}
 
 	#Retour erreur
@@ -55,12 +73,21 @@ case $choice in
 	        read host
 	        echo "Veuillez saisir l'adresse IP de la machine :"
         	read ip
+
+		#Verification de l'adresse IP
+		verif_ip
+
+        	echo "Veuillez saisir le nom d'utilisateur de la machine :"
+        	read user
+
+		#Initialisation de la connxion SSH
+		echo "Verification des informations en cours... (Ctrl+D pour quitter la machine une fois connecté et continuer le script)"
+		ssh_timeout
+
         	echo "Veuillez saisir le nom de la distribution :"
         	read distribution
         	echo "Veuillez saisir l'OS de la machine :"
         	read os
-        	echo "Veuillez saisir le nom d'utilisateur de la machine :"
-        	read user
         	echo "Environnement de la machine (prod, recette, developpement) ?"
         	read envi
 		ADD="$host"":""$ip"":""$distribution"":""$os"":""$user"":""$envi"
