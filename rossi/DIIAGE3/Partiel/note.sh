01b744dd922eee22107af2885db1c64fda313e3b
Benjamin Rossi <Benjamin.benjamin.rossi@diiage.org>
2018-10-09 09:22:09 +0200

    Edit note.sh with elif

--- rossi/DIIAGE3/Partiel/note.sh
+++ rossi/DIIAGE3/Partiel/note.sh
@@ -16,36 +16,32 @@ fi
 #Test if $note > 10 ==> Error
 while [ "$note" -gt 10 ]
 do
-	echo "[ERROR] You score is not in the range 0 to 10 !"
+	echo "[ERROR] You score is not in the range 0 to 10 ! Enter a new score :"
 	read -r note
 done
 
 #Test if $note >=9
-	if [ $note -ge 9 ]
+	if [ "$note" -ge 9 ]
 	then
 		echo "Perfect"
-	fi
 
 #Test if $note >=7
-	if [ $note -ge 7 ]
+	elif [ "$note" -ge 7 ]
 	then
 		echo "Good"
-	fi
 
 #Test if $note >=5
-	if [ $note -ge 5 ]
+	elif [ "$note" -ge 5 ]
 	then
 		echo "Not bad"
-	fi
 
 #Test if $note >=2 : Je n'arrive pas a mettre la decimale
-	if [ $note -ge 2 ]
+	elif [ "$note" -ge 2 ]
 	then
 		echo "Not good"
-	fi
 
 #Test if $note <2 : Je n'arrive pas a mettre la decimale
-	if [ $note -lt 2 ]
+	elif [ "$note" -lt 2 ]
 	then
 		echo "Bad"
 	fi
